ccde7a6669187bf1858539e7446b1efabbae57c6
Anjani Kumar <anjanik012@gmail.com>
2020-05-05 20:27:23 +0530

    Fixed some types

--- random_salt.h
+++ random_salt.h
@@ -14,10 +14,10 @@ class random_salt {
 private:
     static const char symbols[65];
     random_device rng;
-    uniform_int_distribution<> dist;
+    uniform_int_distribution<uint8_t> dist;
 public:
     random_salt(): dist(0, 63){}
-    std::string get_salt(const uint8_t);
+    std::string get_salt(uint8_t);
 };
 
 
