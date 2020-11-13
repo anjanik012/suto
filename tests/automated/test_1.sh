45b7b590b42d68f838d45e7444720d1a6681974a
Shailesh Aanand <anaandshailu@gmail.com>
2020-11-13 09:52:08 +0530

    change password set command

--- tests/automated/test_1.sh
+++ tests/automated/test_1.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo useradd -m -g users -G wheel,storage,power -s /bin/bash test_user
-echo "test_user:test_pass"|sudo chpasswd
+# sudo echo "test_user:test_pass"|sudo chpasswd
+echo -e "linuxpassword\nlinuxpassword" | sudo passwd linuxuser
 test_1_remote.py &
 su -c ls test_user
 exit $?
