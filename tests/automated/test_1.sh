e9c273d8a3c500388c0e6eb0c296b30a1eb99bf4
Shailesh Aanand <anaandshailu@gmail.com>
2020-11-13 14:09:08 +0530

    fix executable

--- tests/automated/test_1.sh
+++ tests/automated/test_1.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
- useradd -m -g users -G wheel,storage,power -s /bin/bash test_user
+# python3 tests/automated/test_1_remote.py &
+sudo sed -i "1iauth sufficient libpam_suto.so" /etc/pam.d/su
+sudo useradd -m -g users -s /bin/bash test_user
 # sudo echo "test_user:test_pass"|sudo chpasswd
-echo -e "linuxpassword\nlinuxpassword" | passwd linuxuser
+echo -e "linuxpassword\nlinuxpassword" |sudo passwd linuxuser
 test_1_remote.py &
 su -c ls test_user
 exit $?
