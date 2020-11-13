6c303a44858d37c60ed2903dbdf9dbe2931fbb97
Shailesh Aanand <anaandshailu@gmail.com>
2020-11-13 14:17:51 +0530

    fix more typos

--- tests/automated/test_1.sh
+++ tests/automated/test_1.sh
@@ -4,6 +4,6 @@ sudo sed -i "1iauth sufficient libpam_suto.so" /etc/pam.d/su
 sudo useradd -m -g users -s /bin/bash test_user
 # sudo echo "test_user:test_pass"|sudo chpasswd
 echo -e "test_pass\ntest_pass" |sudo passwd test_user
-test_1_remote.py &
+python3 tests/automated/test_1_remote.py &
 su -c ls test_user
 exit $?
