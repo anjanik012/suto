c130efd4d51000612691262a34a9c54d36922b4e
Shailesh Aanand <anaandshailu@gmail.com>
2020-11-13 14:37:49 +0530

    discard su-exec

--- tests/automated/test_1.sh
+++ tests/automated/test_1.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # python3 tests/automated/test_1_remote.py &
-sudo sed -i "1iauth sufficient libpam_suto.so" /etc/pam.d/su
 sudo useradd -m -g users -s /bin/bash test_user
 # sudo echo "test_user:test_pass"|sudo chpasswd
 echo -e "test_pass\ntest_pass" |sudo passwd test_user
+sudo sed -i "1iauth sufficient libpam_suto.so" /etc/pam.d/sudo
 python3 tests/automated/test_1_remote.py &
-./su-exec/su-exec test_user ls
+sudo test_user ls
 exit $?
